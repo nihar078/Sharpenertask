3c8960e523a07e371818cf47bea159b653b810d8
nihar078 <nihar078@gmail.com>
2023-03-17 19:08:22 +0530

    adding Task_1

---
+++ Task_1/task1.js
@@ -0,0 +1,7 @@
+let header = document.getElementById("main-header")
+header.style.borderBottom = "solid 3px #000"
+
+let titles =document.querySelectorAll(".title")
+// console.log(titles)
+titles[0].style.color = "green"
+titles[0].innerHTML = "<b>Add Items</b>"
