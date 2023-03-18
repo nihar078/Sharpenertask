0d9fc23facfa1990989f9eb69bfedbac6db407bf
nihar078 <nihar078@gmail.com>
2023-03-18 09:28:23 +0530

    Adding Task_3

---
+++ Task_3/task3.js
@@ -0,0 +1,9 @@
+let it = document.getElementsByClassName("list-item")
+// console.log(it) 
+// console.log(it[0])
+it[0].style.fontWeight = "bold"
+
+let li = document.getElementsByTagName("li")
+console.log(li)
+li[4].style.color = "blue"
+li[4].style.backgroundColor = "yellow"
