fccbda99719a5886481f0606a2bd5f924fd3025f
nihar078 <nihar078@gmail.com>
2023-03-21 17:09:35 +0530

    Adding Task-10

---
+++ Task_10/task10.js
@@ -0,0 +1,38 @@
+let details = document.getElementById("details")
+
+details.addEventListener("submit", saveToLocalStorage)
+
+function saveToLocalStorage(e){
+    e.preventDefault()
+    // const name = e.target.name.value
+    const name = document.getElementById("name").value
+    // const email = e.target.mail.value
+    const email = document.getElementById("mail").value
+    // const phone = e.target.phone.value
+    const phone = document.getElementById("phone").value
+
+    // localStorage.setItem("name", name)
+    // localStorage.setItem("email", email)
+    // localStorage.setItem("phone", phone)
+    const obj = {
+        name,
+        email,
+        phone
+    }
+    localStorage.setItem("userDetails", (JSON.stringify(obj)))
+    showUserOnScreen(obj)
+}
+function showUserOnScreen(obj){
+    let parentElem = document.getElementById("listOfItems")
+    if (!parentElem) {
+        parentElem = document.createElement("ul");
+        parentElem.id = "listOfItems";
+        document.body.appendChild(parentElem);
+      }
+
+    let childElem = document.createElement("li")
+    childElem.textContent = obj.name + " - " + obj.email + " - " + obj.phone
+    parentElem.appendChild(childElem)
+    // parentElem.innerHTML = parentElem.innerHTML + `<li>${obj.name} - ${obj.email} - ${obj.phone}</li>`
+
+}
